70b641542b388c7e7db0e138c87680db264273f5
Markus Winkler <winkler.info@icloud.com>
2024-12-11 18:13:39 +0100

    multi scanner

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,61 @@
+import inspect
+import json
+import logging
+import os
+import sys
+from datetime import datetime
+from time import sleep
+
+import amaas.grpc
 import boto3
+import requests
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
+
+_LOGGER = logging.getLogger()
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s (%(threadName)s) [%(funcName)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 app = Flask(__name__)
 CORS(app)
 
 s3 = boto3.client("s3")
 
-BUCKET_NAME = "pgo-id-scanning-bucket-1o3ky4jp"
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+if BUCKET_NAME == "" or BUCKET_NAME is None:
+    BUCKET_NAME = "pgo-id-scanning-bucket-1o3ky4jp"
+
+V1_REGION = os.getenv("V1_REGION")
+if V1_REGION == "" or V1_REGION is None:
+    V1_REGION = "us-east-1"
+V1_API_KEY = os.getenv("V1_API_KEY")
+if V1_REGION == "us-east-1":
+    API_BASE_URL = "https://api.xdr.trendmicro.com"
+else:
+    API_BASE_URL = f"https://api.{V1_REGION}.xdr.trendmicro.com"
+
+# FSS
+FSS_TAG_PREFIX = "filesecurity-"
+FSS_PML = True
+FSS_FEEDBACK = True
+FSS_VERBOSE = True
+FSS_DIGEST = True
+# /FSS
+
+# SANDBOX
+SB_TAG_PREFIX = "sandbox-"
+URL_PATH_ANALYZE = "/v3.0/sandbox/files/analyze"
+URL_PATH_TASKS = "/v3.0/sandbox/tasks"
+HEADERS = {"Authorization": "Bearer " + V1_API_KEY}
+QUERY_PARAMS = {}
+# /SANDBOX
 
 
 @app.route("/")
@@ -23,7 +70,7 @@ def static_proxy(path):
 
 @app.route("/api/data", methods=["GET"])
 def get_data():
-    return jsonify({"message": "Wating for files to upload!"})
+    return jsonify({"message": "Let me crunch your files for you!"})
 
 
 @app.route("/api/uploadfile", methods=["POST"])
@@ -40,5 +87,267 @@ def upload_s3():
     return jsonify({"message": "OK"})
 
 
+@app.route("/api/scansandbox", methods=["POST"])
+def scan_sandbox():
+    img = request.files["file"]
+    if img:
+        filename = secure_filename(img.filename)
+        try:
+            task_id = sandbox_submit(key=filename, buffer=img.read())
+            sandbox_analysis_results = sandbox_wait_for_result(task_id)
+            tags = sandbox_get_analysis_results(sandbox_analysis_results)
+        except Exception as ex:
+            return jsonify({"message": "Error", "error": str(ex.text)})
+
+    return jsonify({"message": "OK", "tags": tags})
+
+
+@app.route("/api/scanfs", methods=["POST"])
+def scan_fs():
+    img = request.files["file"]
+    if img:
+        filename = secure_filename(img.filename)
+        _LOGGER.info(filename)
+        try:
+            tags = fss_submit(key=filename, buffer=img.read())
+            _LOGGER.info(tags)
+        except Exception as ex:
+            return jsonify({"message": "Error", "error": str(ex)})
+
+    return jsonify({"message": "OK", "tags": tags})
+
+
+@app.route("/api/scancs", methods=["POST"])
+def scan_cs():
+    img = request.files["file"]
+    if img:
+        filename = secure_filename(img.filename)
+        img.save(f"./{filename}")
+
+    return jsonify({"message": "OK"})
+
+
+# #############################################################################
+# File Security Handling
+# #############################################################################
+def fss_submit(key, buffer):
+    handle = amaas.grpc.init_by_region(V1_REGION, V1_API_KEY, True)
+    _LOGGER.info("FSS SCAN")
+    try:
+        scan_resp = amaas.grpc.scan_buffer(
+            handle,
+            buffer,
+            key,
+            tags=["pgo"],
+            pml=FSS_PML,
+            feedback=FSS_FEEDBACK,
+            verbose=FSS_VERBOSE,
+            digest=FSS_DIGEST,
+        )
+    except Exception as e:
+        _LOGGER.error(e, extra={"function": inspect.currentframe().f_code.co_name})
+        _LOGGER.error(
+            "Error scanning object {}.".format(key),
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+
+    response = json.loads(scan_resp)
+
+    scanning_result = response.get("result")
+    _LOGGER.info(scanning_result)
+    findings = scanning_result.get("atse").get("malwareCount")
+    _LOGGER.info(findings)
+    scan_result = "malicious" if findings else "no issues found"
+    _LOGGER.info(scan_result)
+    scan_date = datetime.strftime(
+        datetime.fromisoformat(response.get("timestamp").get("end")),
+        "%m/%d/%Y %H:%M:%S",
+    )
+    _LOGGER.info(scan_date)
+    malware_name = (
+        scanning_result.get("atse").get("malware")[0].get("name") if findings else "n/a"
+    )
+    _LOGGER.info(malware_name)
+
+    tags = [
+        f"{FSS_TAG_PREFIX}scanned=true",
+        f"{FSS_TAG_PREFIX}scan-date={scan_date}",
+        f"{FSS_TAG_PREFIX}scan-result={scan_result}",
+        f"{FSS_TAG_PREFIX}scan-detail-code={malware_name}",
+    ]
+
+    amaas.grpc.quit(handle)
+
+    _LOGGER.info(tags)
+
+    return tags
+
+
+# #############################################################################
+# Sandbox Handling
+# #############################################################################
+def sandbox_submit(key, buffer):
+
+    data = {
+        #    'documentPassword': 'YOUR_DOCUMENTPASSWORD (base64-encoded characters)',
+        #    'archivePassword': 'YOUR_ARCHIVEPASSWORD (base64-encoded characters)',
+        #    'arguments': 'YOUR_ARGUMENTS (base64-encoded characters)'
+    }
+    files = {"file": (key, buffer, "application/octet-stream")}
+    try:
+        response = requests.post(
+            API_BASE_URL + URL_PATH_ANALYZE,
+            params=QUERY_PARAMS,
+            headers=HEADERS,
+            data=data,
+            files=files,
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        _LOGGER.error(
+            errh.args[0], extra={"function": inspect.currentframe().f_code.co_name}
+        )
+        raise
+    except requests.exceptions.ReadTimeout:
+        _LOGGER.error(
+            "Time out", extra={"function": inspect.currentframe().f_code.co_name}
+        )
+        raise
+    except requests.exceptions.ConnectionError:
+        _LOGGER.error(
+            "Connection error",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+        raise
+    except requests.exceptions.RequestException:
+        _LOGGER.error(
+            "Exception request",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+        raise
+
+    task_id = response.json().get("id")
+
+    _LOGGER.info(
+        f"Sandbox Task ID: {task_id}",
+        extra={"function": inspect.currentframe().f_code.co_name},
+    )
+
+    return task_id
+
+
+def sandbox_wait_for_result(task_id):
+
+    sandbox_analysis_results = None
+    sandbox_analysis_failed = False
+    while sandbox_analysis_results is None and not sandbox_analysis_failed:
+        try:
+            response = requests.get(
+                API_BASE_URL + URL_PATH_TASKS + "/" + task_id,
+                params=QUERY_PARAMS,
+                headers=HEADERS,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            _LOGGER.error(
+                errh.args[0], extra={"function": inspect.currentframe().f_code.co_name}
+            )
+            raise
+        except requests.exceptions.ReadTimeout:
+            _LOGGER.error(
+                "Time out", extra={"function": inspect.currentframe().f_code.co_name}
+            )
+            raise
+        except requests.exceptions.ConnectionError:
+            _LOGGER.error(
+                "Connection error",
+                extra={"function": inspect.currentframe().f_code.co_name},
+            )
+            raise
+        except requests.exceptions.RequestException:
+            _LOGGER.error(
+                "Exception request",
+                extra={"function": inspect.currentframe().f_code.co_name},
+            )
+            raise
+
+        task_result = response.json()
+        if task_result.get("status") == "succeeded":
+            sandbox_analysis_results = task_result.get("resourceLocation")
+        if task_result.get("status") == "failed":
+            sandbox_analysis_failed = True
+        _LOGGER.info(
+            "Sandbox Analysis running",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+        sleep(10)
+
+    if sandbox_analysis_failed:
+        _LOGGER.error(
+            "Sandbox Analysis failed",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+
+    return sandbox_analysis_results
+
+
+def sandbox_get_analysis_results(sandbox_analysis_results):
+    try:
+        response = requests.get(
+            sandbox_analysis_results, params=QUERY_PARAMS, headers=HEADERS
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        _LOGGER.error(
+            errh.args[0], extra={"function": inspect.currentframe().f_code.co_name}
+        )
+        raise
+    except requests.exceptions.ReadTimeout:
+        _LOGGER.error(
+            "Time out", extra={"function": inspect.currentframe().f_code.co_name}
+        )
+        raise
+    except requests.exceptions.ConnectionError:
+        _LOGGER.error(
+            "Connection error",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+        raise
+    except requests.exceptions.RequestException:
+        _LOGGER.error(
+            "Exception request",
+            extra={"function": inspect.currentframe().f_code.co_name},
+        )
+        raise
+
+    analysis_result = response.json()
+
+    detection_names = " ".join(analysis_result.get("detectionNames", ["n/a"]))
+    threat_types = " ".join(analysis_result.get("threatTypes", ["n/a"]))
+    scan_date = datetime.strftime(
+        datetime.fromisoformat(analysis_result.get("analysisCompletionDateTime")),
+        "%m/%d/%Y %H:%M:%S",
+    )
+    tags = [
+        f"{SB_TAG_PREFIX}risk-level={analysis_result.get('riskLevel', 'n/a')}",
+        f"{SB_TAG_PREFIX}detection-names={detection_names}",
+        f"{SB_TAG_PREFIX}threat-type={threat_types}",
+        f"{SB_TAG_PREFIX}analysis-completed={scan_date}",
+    ]
+
+    _LOGGER.info(tags)
+
+    return tags
+
+
 if __name__ == "__main__":
+    _LOGGER.info(f"V1 Region: {V1_REGION}")
+    _LOGGER.info(f"API Key: ...{V1_API_KEY[-4:]}")
+    _LOGGER.info(f"API Base URL: {API_BASE_URL}")
+    _LOGGER.info(f"Bucket Name: {BUCKET_NAME}")
+    _LOGGER.info(f"AWS Access Key: {os.getenv('AWS_ACCESS_KEY_ID', '')}")
+    _LOGGER.info(
+        f"AWS Secret Key: {"".ljust(len(os.getenv('AWS_SECRET_ACCESS_KEY', '')), '*')}"
+    )
+
     app.run(debug=True, host="0.0.0.0")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import { Button, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import scientistLogo from "./assets/Mad_scientist_transparent_background.svg";
 
 function App() {
   const [data, setData] = useState(null);
@@ -11,8 +12,9 @@ function App() {
   const [responseData, setResponseData] = useState(null);
 
   // Empty string when running in container
-  const BASEURL = "http://192.168.1.122:5000"
-  
+  const BASEURL = "http://192.168.1.122:5000";
+  // const BASEURL = ""
+
   useEffect(() => {
     axios
       .get(BASEURL + "/api/data")
@@ -27,6 +29,8 @@ function App() {
   const onFileUpload = async () => {
     if (selectedFile) {
       const formData = new FormData();
+      setResponseData({ message: "Uploading..." });
+
       formData.append("file", selectedFile, selectedFile.name);
 
       console.log("File Name:", selectedFile.name);
@@ -57,18 +61,117 @@ function App() {
     }
   };
 
+  const onFileScanSandbox = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      setResponseData({ message: "Uploading..." });
+
+      formData.append("file", selectedFile, selectedFile.name);
+
+      console.log("File Name:", selectedFile.name);
+      console.log("File Type:", selectedFile.type);
+      console.log("File Size:", selectedFile.size, "bytes");
+      try {
+        const response = await axios.post(
+          BASEURL + "/api/scansandbox",
+          formData
+        );
+        // Parse the response
+        setResponseData(response.data);
+        console.log("Response Data:", response.data);
+
+        if (response.data.message) {
+          console.log("Success Message:", response.data.message);
+        } else {
+          console.log("Error Message:", response.data.error);
+        }
+      } catch (error) {
+        console.error(
+          "Error:",
+          error.response ? error.response.error : error.message
+        );
+      }
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  const onFileScanFS = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      setResponseData({ message: "Uploading..." });
+
+      formData.append("file", selectedFile, selectedFile.name);
+
+      console.log("File Name:", selectedFile.name);
+      console.log("File Type:", selectedFile.type);
+      console.log("File Size:", selectedFile.size, "bytes");
+      try {
+        const response = await axios.post(BASEURL + "/api/scanfs", formData);
+        // Parse the response
+        setResponseData(response.data);
+        console.log("Response Data:", response.data);
+
+        if (response.data.message) {
+          console.log("Success Message:", response.data.message);
+        } else {
+          console.log("Error Message:", response.data.error);
+        }
+      } catch (error) {
+        console.error(
+          "Error:",
+          error.response ? error.response.error : error.message
+        );
+      }
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  const onFileScanCS = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      setResponseData({ message: "Uploading..." });
+
+      formData.append("file", selectedFile, selectedFile.name);
+
+      console.log("File Name:", selectedFile.name);
+      console.log("File Type:", selectedFile.type);
+      console.log("File Size:", selectedFile.size, "bytes");
+      try {
+        const response = await axios.post(BASEURL + "/api/scancs", formData);
+        // Parse the response
+        setResponseData(response.data);
+        console.log("Response Data:", response.data);
+
+        if (response.data.message) {
+          console.log("Success Message:", response.data.message);
+        } else {
+          console.log("Error Message:", response.data.error);
+        }
+      } catch (error) {
+        console.error(
+          "Error:",
+          error.response ? error.response.error : error.message
+        );
+      }
+    } else {
+      console.log("No file selected");
+    }
+  };
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://mawinkler.github.io/playground-one-pages/"
+          target="_blank"
+        >
+          <img src={scientistLogo} className="logo" alt="Scientific Scanner" />
         </a>
       </div>
       <div>
-        <h1>S3 File Uploader</h1>
+        <h1>File Uploader and Scanner</h1>
         <p>{data ? data.message : "Loading..."}</p>
       </div>
       <div>
@@ -90,10 +193,41 @@ function App() {
           onClick={onFileUpload}
           style={{ marginTop: 20 }}
         >
-          Upload
+          Upload to S3
         </Button>
-        {/* {responseData && <pre>{JSON.stringify(responseData.message, null, 2)}</pre>} */}
-        {responseData && <pre>{responseData.message}</pre>}
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onFileScanSandbox}
+          style={{ marginTop: 20 }}
+        >
+          Sandbox
+        </Button>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onFileScanFS}
+          style={{ marginTop: 20 }}
+        >
+          File Security
+        </Button>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onFileScanCS}
+          style={{ marginTop: 20 }}
+        >
+          Container Security
+        </Button>
+
+        {/* {responseData && <pre>{JSON.stringify(responseData.tags, null, 2)}</pre>} */}
+        {responseData && <pre>Message: {responseData.message}</pre>}
+        {responseData && responseData.tags && (
+          <pre>Tags: {JSON.stringify(responseData.tags, null, 2)}</pre>
+        )}
       </div>
     </>
   );
